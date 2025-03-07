8a9ed969ecedf3177b28231b745b850b86eae0ef
TamasRaj <rajat.katiyar987@gmail.com>
2025-03-07 15:17:04 +0530

    updated add function to handle new line in input

--- calculator.py
+++ calculator.py
@@ -1,8 +1,17 @@
 def add(numbers_str: str) -> int:
     if numbers_str == "":
         return 0
+
     if "," in numbers_str:
         numbers = numbers_str.split(",")
+        
         # Treat empty string to be 0 hence ignore
         return sum([int(number) for number in numbers if number != ""])
+    
+    if "\n" in numbers_str:
+        numbers = numbers_str.split("\n")
+
+        # Treat empty string to be 0 hence ignore
+        return sum([int(number) for number in numbers if number != ""])
+
     return int(numbers_str)
