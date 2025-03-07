2f4504655bf30198e99041bd498b0e3594c0f363
TamasRaj <rajat.katiyar987@gmail.com>
2025-03-07 15:32:26 +0530

    Refactored add function

--- calculator.py
+++ calculator.py
@@ -1,18 +1,15 @@
 def add(numbers_str: str) -> int:
     if numbers_str == "":
         return 0
+
+    if "," not in numbers_str and "\n" not in numbers_str:
+        return int(numbers_str)
     
+    delimiter = ","
     if numbers_str.startswith("//"):
         delimiter, numbers_str = numbers_str.split("\n", 1)
         delimiter = delimiter[2:]
-        numbers = numbers_str.split(delimiter)
-        return sum([int(number) for number in numbers if number != ""])
-
-    if "," in numbers_str or "\n" in numbers_str:
-        numbers_str = numbers_str.replace("\n", ",")
-        numbers = numbers_str.split(",")
-        
-        # Treat empty string to be 0 hence ignore
-        return sum([int(number) for number in numbers if number != ""])
-
-    return int(numbers_str)
+    
+    numbers_str = numbers_str.replace("\n", delimiter)
+    numbers = numbers_str.split(delimiter)
+    return sum([int(number) for number in numbers if number != ""])
