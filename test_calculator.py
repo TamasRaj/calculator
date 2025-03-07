a6bd60aaa7f51d205bae05c2367075284af168d2
TamasRaj <rajat.katiyar987@gmail.com>
2025-03-07 13:36:07 +0530

    added test for value 1

--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(calculator.add(""), 0)
+        self.assertEqual(calculator.add("1"), 1)
 
 if __name__ == '__main__':
     unittest.main()
