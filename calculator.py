a23396b1aafc7a5d7ee87c0ebe55fde6c52dda23
TamasRaj <rajat.katiyar987@gmail.com>
2025-03-07 15:20:07 +0530

    updated add funtion to handle newline and commas together

--- calculator.py
+++ calculator.py
@@ -2,15 +2,10 @@ def add(numbers_str: str) -> int:
     if numbers_str == "":
         return 0
 
-    if "," in numbers_str:
+    if "," in numbers_str or "\n" in numbers_str:
+        numbers_str = numbers_str.replace("\n", ",")
         numbers = numbers_str.split(",")
         
-        # Treat empty string to be 0 hence ignore
-        return sum([int(number) for number in numbers if number != ""])
-    
-    if "\n" in numbers_str:
-        numbers = numbers_str.split("\n")
-
         # Treat empty string to be 0 hence ignore
         return sum([int(number) for number in numbers if number != ""])
 
