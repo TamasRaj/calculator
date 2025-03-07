de377ec8111e2bfd62b0e78765d54eaf741229da
TamasRaj <rajat.katiyar987@gmail.com>
2025-03-07 13:40:14 +0530

    updated add function to add 2 numbers

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
 def add(numbers_str: str) -> int:
     if numbers_str == "":
         return 0
+    if "," in numbers_str:
+        numbers = numbers_str.split(",")
+        return int(numbers[0]) + int(numbers[1])
     return int(numbers_str)
