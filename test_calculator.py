5bae013b17774a5aad13ea3ec1ee63bed1d3b0e6
TamasRaj <rajat.katiyar987@gmail.com>
2025-03-07 13:33:29 +0530

    Added empty string test case

--- test_calculator.py
+++ test_calculator.py
@@ -1,9 +1,13 @@
 import unittest
+import calculator
 
 class TestCalculator(unittest.TestCase):
 
     def test_nothing(self):
         pass
+
+    def test_add(self):
+        self.assertEqual(calculator.add(""), 0)
 
 if __name__ == '__main__':
     unittest.main()
